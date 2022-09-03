70b78dbaa7ee184156b0b046cdce977cccea2f8f
tomkcey <tomkcey@gmail.com>
2022-09-02 23:53:21 -0400

    made install script better

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
+$SOURCE=$HOME/.config/.dotfiles
+$CONFIG=$HOME/.config
+
 sudo pacman -S --needed zsh
-ln -s .zshrc ~/
+ln -s $SOURCE/.zshrc $HOME/.zshrc
 source ~/.zshrc
 
 
@@ -9,17 +12,23 @@ sudo pacman -S --needed neofetch neovim polybar tmux starship
 cargo install alacritty
 pacman -S --needed cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python blueman bluez bluez-libs bluez-utils pamixer pavucontrol nitrogen feh gsettings pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire wireplumber yarn picom htop
 
-mv nordtheme.png ~/Pictures/
-mv monitor.sh ~/.screenlayout/
+mv $SOURCE/nordtheme.png $HOME/Pictures/
+mv $SOURCE/monitor.sh $HOME/.screenlayout/
 
-mkdir ~/.local/share/fonts
-ln -s fonts/* ~/.local/share/fonts/
-ln -s i3/* ~/.config/i3/
-mkdir ~/.config/alacritty
-ln -s alacritty/* ~/.config/alacritty/
-ln -s .tmux.conf ~/.tmux.conf
-ln -s starship.toml ~/.config/starship.toml
-ln -s neofetch/* ~/.config/neofetch/
-ln -s polybar/* ~/.config/polybar/
-ln -s nvim/* ~/.config/nvim/
-ln -s autostart/* ~/.config/autostart/
+mkdir $HOME/.local/share/fonts
+mkdir $HOME/.config/alacritty
+mkdir $HOME/.config/polybar
+mkdir $HOME/.config/nvim
+
+rm $HOME/.config/i3/config
+rm $HOME/.config/neofetch/config.conf
+
+ln -s $SOURCE/fonts/* $HOME/.local/share/fonts/
+ln -s $SOURCE/i3/* $CONFIG/.config/i3/
+ln -s $SOURCE/alacritty/* $CONFIG/.config/alacritty/
+ln -s $SOURCE/.tmux.conf $HOME/.tmux.conf
+ln -s $SOURCE/starship.toml $CONFIG/.config/starship.toml
+ln -s $SOURCE/neofetch/* $CONFIG/.config/neofetch/
+ln -s $SOURCE/polybar/* $CONFIG/.config/polybar/
+ln -s $SOURCE/nvim/* $CONFIG/.config/nvim/
+ln -s $SOURCE/autostart/* $CONFIG/.config/autostart/
