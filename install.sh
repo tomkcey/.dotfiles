e125e17196c6b4a26e393c3491c5719eaf6b6463
tomkcey <tomkcey@gmail.com>
2022-09-02 21:46:07 -0400

    committin'

--- install.sh
+++ install.sh
@@ -7,9 +7,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | sh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo pacman -S --needed neofetch neovim polybar tmux starship
 cargo install alacritty
-pacman -S --needed cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python blueman bluez bluez-libs bluez-utils pamixer pavucontrol
+pacman -S --needed cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python blueman bluez bluez-libs bluez-utils pamixer pavucontrol nitrogen feh gsettings
 
 mv nordtheme.png ~/Pictures/
+mv monitor.sh ~/.screenlayout/
 
 ln -s fonts/* ~/.local/share/fonts/
 ln -s i3/* ~/.config/i3/
