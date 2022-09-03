0ca9eb76ce3d44d6f11ee7dea2b65857b41211c9
tomkcey <tomkcey@gmail.com>
2022-09-02 22:06:24 -0400

    another

--- install.sh
+++ install.sh
@@ -7,13 +7,15 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | sh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo pacman -S --needed neofetch neovim polybar tmux starship
 cargo install alacritty
-pacman -S --needed cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python blueman bluez bluez-libs bluez-utils pamixer pavucontrol nitrogen feh gsettings
+pacman -S --needed cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python blueman bluez bluez-libs bluez-utils pamixer pavucontrol nitrogen feh gsettings pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire wireplumber yarn picom htop
 
 mv nordtheme.png ~/Pictures/
 mv monitor.sh ~/.screenlayout/
 
+mkdir ~/.local/share/fonts
 ln -s fonts/* ~/.local/share/fonts/
 ln -s i3/* ~/.config/i3/
+mkdir ~/.config/alacritty
 ln -s alacritty/* ~/.config/alacritty/
 ln -s .tmux.conf ~/.tmux.conf
 ln -s starship.toml ~/.config/starship.toml
